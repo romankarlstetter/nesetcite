3b197f1975b035a9c1580c097b36260d794c17c5
Roman Karlstetter <roman.karlstetter@student.ecp.fr>
2012-10-25 16:45:49 +0200

    enable profile view

--- workspace/NesEtCite/src/fr/ecp/innovationprj/nesetcite/youngpeople/ProfileActivity.java
+++ workspace/NesEtCite/src/fr/ecp/innovationprj/nesetcite/youngpeople/ProfileActivity.java
@@ -3,16 +3,34 @@ package fr.ecp.innovationprj.nesetcite.youngpeople;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TabHost;
 import fr.ecp.innovationprj.nesetcite.R;
+import fr.ecp.innovationprj.nesetcite.TabsAdapter;
 
 public class ProfileActivity extends FragmentActivity {
-	
+    TabHost mTabHost;
+    ViewPager  mViewPager;
+    TabsAdapter mTabsAdapter;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_tabs_pager);
+        mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+        
+        mViewPager = (ViewPager)findViewById(R.id.pager);
+        
+        mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
+        
+        mTabsAdapter.addTab(mTabHost.newTabSpec("profile").setIndicator("Mon Profile"), 
+                ProfileFragment.class, null);
+        mTabsAdapter.addTab(mTabHost.newTabSpec("cv").setIndicator("Mon CV"), DummyFragment.class, null);
+        mTabsAdapter.addTab(mTabHost.newTabSpec("cv2").setIndicator("BLuB"), DummyFragment.class, null);
+        
     }
 
     @Override
